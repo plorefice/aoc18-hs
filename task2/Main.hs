5a08ccfa750871667f55028744f76b7bfcdb5c54
Pietro Lorefice <pietro.lorefice@gmail.com>
2018-12-02 11:37:19 +0100

    task2: solution to problem B

--- task2/Main.hs
+++ task2/Main.hs
@@ -6,8 +6,19 @@ import           Data.Function
 main :: IO ()
 main = do
   input <- getContents
-  print . uncurry (*) . foldl countIDs (0, 0) . lines $ input
+  let xs = lines input
+  print . uncurry (*) . foldl countIDs (0, 0) $ xs -- Task A
+  print
+    . map fst
+    . filter (uncurry (==))
+    . uncurry zip
+    . head
+    . filter differByOne
+    $ [ (a, b) | a <- xs, b <- xs, a /= b ] -- Task B
 
 countIDs :: (Int, Int) -> String -> (Int, Int)
 countIDs (n2, n3) id = (n2 + f 2, n3 + f 3)
   where f n = min 1 . length . filter ((== n) . length) . group . sort $ id
+
+differByOne :: (String, String) -> Bool
+differByOne (a, b) = (== 1) . length . filter (uncurry (/=)) $ zip a b
